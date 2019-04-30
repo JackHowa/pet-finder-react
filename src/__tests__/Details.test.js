93f94dda7fcb6283f72408d832b564120fa53bb2
Jack Howard <jackhowaa@gmail.com>
2019-04-30 09:43:01 -0500

    Add basic snapshot and unit test

---
+++ src/__tests__/Details.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { create } from "react-test-renderer";
+import { Details } from "../Details";
+
+test("snapshot", () => {
+  // if anything changes, then will fail
+  // prevents regressions
+  // use -u to update
+  const c = create(<Details />);
+  expect(c.toJSON()).toMatchSnapshot();
+});
+
+test("shows modal when toggle modal", () => {
+  const c = create(<Details search={() => {}} />);
+  const instance = c.getInstance();
+
+  expect(instance.state.showModal).toBe(false);
+  instance.toggleModal();
+  expect(instance.state.showModal).toBe(true);
+});
