4d87b0b9e2e626eb4500e1caf5b8af20ff3d2ea5
Jack Howard <jackhowaa@gmail.com>
2019-04-30 15:26:08 -0500

    Replace loadable with suspense, lazy and add redux

--- src/App.js
+++ src/App.js
@@ -1,79 +1,29 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { render } from "react-dom";
 import { Router } from "@reach/router";
-import Loadable from "react-loadable";
-import Results from "./Results";
-import { SearchParams } from "./SearchParams";
-import pf from "petfinder-client";
-import { Provider } from "./SearchContext";
-import NavBar from "./NavBar";
+import { Provider } from "react-redux";
 
-const LoadableDetails = Loadable({
-  // split out details into a separate bundle
-  loader: () => import("./Details"),
-  loading: () => <h1>loading split out code</h1>
-});
+import Results from "./Results";
+import NavBar from "./NavBar";
+import store from "./store";
+
+const SearchParams = lazy(() => import("./SearchParams"));
+const Details = lazy(() => import("./Details"));
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      location: "",
-      animal: "",
-      breed: "",
-      breeds: [],
-      handleBreedChange: this.handleBreedChange,
-      handleLocationChange: this.handleLocationChange,
-      handleAnimalChange: this.handleAnimalChange,
-      getBreeds: this.getBreeds
-    };
-  }
-
-  handleLocationChange = event =>
-    this.setState({ location: event.target.value });
-
-  handleAnimalChange = event =>
-    this.setState({ animal: event.target.value, breed: "" }, this.getBreeds);
-
-  handleBreedChange = event => this.setState({ breed: event.target.value });
-  // doesn't need arrow
-  getBreeds() {
-    if (this.state.animal) {
-      pf()
-        .breed.list({ animal: this.state.animal })
-        .then(data => {
-          if (
-            data.petfinder &&
-            data.petfinder.breeds &&
-            Array.isArray(data.petfinder.breeds.breed)
-          ) {
-            this.setState({
-              breeds: data.petfinder.breeds.breed
-            });
-          } else {
-            this.setState({ breeds: [] });
-          }
-        });
-    } else {
-      this.setState({
-        breeds: []
-      });
-    }
-  }
-
-  handleBreedChange = event => this.setState({ breed: event.target.value });
-
   render() {
     return (
       <div>
         <NavBar />
-        <Provider value={this.state}>
-          <Router>
-            <SearchParams path="/search-params" />
-            <Results path="/" />
-            <LoadableDetails path="/details/:id" />
-          </Router>
-        </Provider>
+        <Suspense fallback={<h1>loading route …</h1>}>
+          <Provider store={store}>
+            <Router>
+              <Results path="/" />
+              <SearchParams path="/search-params" />
+              <Details path="/details/:id" />
+            </Router>
+          </Provider>
+        </Suspense>
       </div>
     );
   }
