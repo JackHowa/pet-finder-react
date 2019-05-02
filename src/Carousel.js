5f0e53fde79e0e5616809a51bcfc13a63d1580c8
Jack Howard <jackhowaa@gmail.com>
2019-05-01 20:01:12 -0500

    Refactor using hooks and useState

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,51 +1,31 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-export class Carousel extends Component {
-  state = {
-    photos: [],
-    active: 0
-  };
+const Carousel = props => {
+  const [active, updateActive] = useState(0);
+  let photos = ["http://placecorgi.com/600/600"];
 
-  // every time props change, state changes
-  // static method exists on class level, not instance level
-  static getDerivedStateFromProps({ media }) {
-    let photos = [];
-
-    if (media && media.photos && media.photos.photo) {
-      photos = media.photos.photo.filter(photo => photo["@size"] === "pn");
-    }
-
-    return { photos };
+  if (props.media && props.media.photos && props.media.photos.photo) {
+    photos = props.media.photos.photo.filter(photo => photo["@size"] === "pn");
   }
 
-  // doesn't create a new context with arrow function
-  // reverts to whatever lexical scope of the instance
-  handleIndexClick = event => {
-    this.setState({ active: Number(event.target.dataset.index) });
-  };
-
-  render() {
-    // want to make render method as simple as possible
-    // using getDerived state
-
-    const { active, photos } = this.state;
-    return (
-      <div className="carousel">
-        <img src={photos[active].value} alt="current-pet" />
-        <div className="carousel-smaller">
-          {photos.map((photo, index) => (
-            /* eslint-disable-next-line */
-            <img
-              key={photo.value}
-              src={photo.value}
-              alt={"animal-thumbnail"}
-              className={active === index ? "active" : ""}
-              data-index={index}
-              onClick={this.handleIndexClick}
-            />
-          ))}
-        </div>
+  return (
+    <div className="carousel">
+      <img src={photos[active].value} alt="current-pet" />
+      <div className="carousel-smaller">
+        {photos.map((photo, index) => (
+          /* eslint-disable-next-line */
+          <img
+            key={photo.value}
+            src={photo.value}
+            alt={"animal-thumbnail"}
+            className={active === index ? "active" : ""}
+            data-index={index}
+            onClick={event => updateActive(Number(event.target.dataset.index))}
+          />
+        ))}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default Carousel;
